21d04b67d1ee3e8102e105e8edb5a2f82598a9b2
Mutayyib HP <mutayyibali@gmail.com>
2023-11-04 00:52:24 +0500

    Taking input from user

---
+++ userInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char firstName[15];
+    char lastName[15];
+    cin >> firstName;
+    cin >> lastName;
+    cout << "Hello " << firstName << " " << lastName << " !!!";
+}
