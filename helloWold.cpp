0f40912df30b9a3f7cf548acbb4334ac6b3a745b
Mutayyib PC <mutayyibali@gmail.com>
2023-11-05 19:49:25 +0500

    Changes on PC

--- helloWold.cpp
+++ helloWold.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     cout << "Hello CS201 !!!" << endl;
-    cout << "Hello Mutayyib Ali";
+    cout << "Hello Mutayyib Ali Sial";
 }
