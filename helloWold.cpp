0c12cd0ef755779ef37ed9e363e3f1c5035cdcab
Mutayyib PC <mutayyibali@gmail.com>
2023-11-16 22:47:51 +0500

    Added file for testing

--- helloWold.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "Hello CS201 !!!" << endl;
-    cout << "Hello Mutayyib Ali Sial";
-}
---
+++ testing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void test()
+{
+}
+
+int main()
+{
+    cout << "Testing!" << endl;
+    test();
+    return 0;
+}
