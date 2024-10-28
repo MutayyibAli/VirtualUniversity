b26dab49050f9a050462e7613447f86ccb4deea4
Mutayyib <mutayyibali@gmail.com>
2024-10-28 22:18:16 +0500

    .

--- CS401Assembly/a02.asm
+++ CS401Assembly/a02.asm
@@ -2,26 +2,26 @@
 
 [org 0x100]
 
-                jmp Start                                       ; Jump to location marked as Start
+                jmp start                                       ; Jump to location marked as Start
 
 data:           dw 5, 8, 45, -15, 67, 15, 9, -2, 14, 0          ; Data to be sorted
 swap:           db 0                                            ; To check if there is no swap in an iteration then sorting is completed
 
-start:          mov bx, 0                                      ; Initiate BX to zero
-                mov [swap], 0                                  ; Reset swap flag to no swap
+start:          mov bx, 0                                       ; Initiate BX to zero
+                mov byte [swap], 0                              ; Reset swap flag to no swap
 
-loop:           mov ax, [data + bx]
+loop1:           mov ax, [data + bx]
                 cmp ax, [data + bx + 2]
                 jbe noswap
 
-swap:           mov dx, [data + bx + 2]
+    b           mov dx, [data + bx + 2]
                 mov [data + bx + 2], ax
                 mov [data + bx], dx
                 mov byte [swap], 1
 
 noswap:         add bx,2
                 cmp bx, 18
-                jne loop
+                jne loop1
 
                 cmp byte [swap], 1
                 je start
