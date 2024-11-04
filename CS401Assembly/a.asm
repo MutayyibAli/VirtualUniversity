2f2608932e15debfa93d6b8dedde8c2e8ad4d9a2
Mutayyib <mutayyibali@gmail.com>
2024-11-04 21:19:10 +0500

    .

--- CS401Assembly/a.asm
+++ CS401Assembly/a.asm
@@ -5,7 +5,8 @@
                 jmp start                                                   ; Jump to the start of the program
 
 firstname:      db 0x4D, 0x75, 0x74, 0x61, 0x79, 0x79, 0x69, 0x62, 0x00     ; "Mutayyib" in ASCII
-count:          db 5
+count:          db 0                                                        ; Count of characters in name
+result:         dw 0                                                        ; Result of the calculation
 
 countletters:   push bp                                                     ; Save the base pointer to stack
                 mov bp, sp                                                  ; Set the base pointer to the stack pointer
@@ -35,6 +36,8 @@ loop2:          shl ax, 1                                                   ; Sh
                 dec bl                                                      ; Decrement the count
                 cmp bl, 0                                                   ; Check if the count is 0
                 jne loop2                                                   ; If not, repeat the loop
+
+                mov [result], ax                                            ; Store the result in the result variable
 
                 mov ax, 0x4C00                                              ; Exit the program
                 int 0x21
