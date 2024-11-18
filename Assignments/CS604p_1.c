2a2ff155de222cd840f8ec40d4c1d8f4db31cadb
Mutayyib <mutayyibali@gmail.com>
2024-11-18 19:49:37 +0500

    CS604P Assignment

--- Assignments/CS604p_1.c
+++ Assignments/CS604p_1.c
@@ -4,11 +4,11 @@
 #include <stdlib.h> // Added for thread compatibility
 
 // Thread function for T1: Prints array and its squares
-void *thread1_func(void *arg)
+void *calculate_squares(void *arg)
 {
     int *array = (int *)arg;
     printf("In First Thread\n");
-    printf("Printing the actual contents of array:\n");
+    printf("Printing the actual array:\n");
     for (int i = 0; i < 10; i++)
     {
         printf("%d\n", array[i]);
@@ -22,7 +22,7 @@ void *thread1_func(void *arg)
 }
 
 // Thread function for T2: Reverses the string
-void *thread2_func(void *arg)
+void *reverse_string(void *arg)
 {
     char *str = (char *)arg;
     int len = strlen(str);
@@ -32,7 +32,7 @@ void *thread2_func(void *arg)
         reversed[i] = str[len - i - 1];
     }
     reversed[len] = '\0';
-    printf("\nIn Second Thread, Printing the Reverse String...\n");
+    printf("\nIn Second Thread, Printing the Reversed string: ");
     printf("%s\n\n", reversed);
     return NULL;
 }
@@ -40,10 +40,10 @@ void *thread2_func(void *arg)
 int main()
 {
     pthread_t thread1, thread2;
-    int array[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int array[10] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
     char str[100];
 
-    // 
+    // Get input string from user and handle errors
     printf("Enter string to pass T2: ");
     if (scanf("%99s", str) != 1)
     {
@@ -51,21 +51,21 @@ int main()
         return 1;
     }
 
-    // Create Thread T1
-    if (pthread_create(&thread1, NULL, thread1_func, array) != 0)
+    // Create Thread T1 for calculating squares and printing them
+    if (pthread_create(&thread1, NULL, calculate_squares, array) != 0)
     {
         fprintf(stderr, "Error creating thread 1.\n");
         return 1;
     }
 
-    // Create Thread T2
-    if (pthread_create(&thread2, NULL, thread2_func, str) != 0)
+    // Create Thread T2 for reversing the string
+    if (pthread_create(&thread2, NULL, reverse_string, str) != 0)
     {
         fprintf(stderr, "Error creating thread 2.\n");
         return 1;
     }
 
-    // Wait for both threads to complete
+    // Wait for both threads to complete and handle errors
     if (pthread_join(thread1, NULL) != 0)
     {
         fprintf(stderr, "Error joining thread 1.\n");
